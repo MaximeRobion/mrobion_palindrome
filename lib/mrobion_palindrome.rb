beff8f1e6b60fcf0f455ca7ade04013c6cf09a19
Maxime Robion <max.robion@hey.com>
2023-06-20 18:15:50 +0200

    Added all test and beautiful code

--- lib/mrobion_palindrome.rb
+++ lib/mrobion_palindrome.rb
@@ -1,8 +1,18 @@
-# frozen_string_literal: true
-
 require_relative "mrobion_palindrome/version"
 
-module MrobionPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    def processed_content
+      # 1. scan:
+        # 1. scan the string
+        # 2. make it an array of only the characters we want
+      # 2. join: bring back the array into a string
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
