2a1bf270bf611dcf0e88c04e5f3e2fd050cbb0ab
Maxime Robion <max.robion@hey.com>
2023-06-20 18:31:48 +0200

    Turn into a module, handle integers

--- lib/mrobion_palindrome.rb
+++ lib/mrobion_palindrome.rb
@@ -1,18 +1,26 @@
 require_relative "mrobion_palindrome/version"
 
-class String
+module MrobionPalindrome
 
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  private
-
-    def processed_content
-      # 1. scan:
-        # 1. scan the string
-        # 2. make it an array of only the characters we want
-      # 2. join: bring back the array into a string
-      self.scan(/[a-z]/i).join.downcase
+    def palindrome?
+      processed_content == processed_content.reverse
     end
+
+    private
+
+      def processed_content
+        # 1. scan:
+          # 1. scan the string
+          # 2. make it an array of only the characters we want
+        # 2. join: bring back the array into a string
+        self.to_s.scan(/[a-z\d]/i).join.downcase
+      end
+end
+
+class String
+  include MrobionPalindrome
+end
+
+class Integer
+  include MrobionPalindrome
 end
